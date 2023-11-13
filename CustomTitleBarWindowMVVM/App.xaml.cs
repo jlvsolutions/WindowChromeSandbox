e421792665c59e92dbb9c59b714b69b6181082f8
Jerry Virden <JerryVirden@outlook.com>
2023-11-12 18:53:17 -0600

    Added second project.  Renamed CustomTitleBarWindow1 to CustomTitleBarWindow.

--- CustomTitleBarWindowMVVM/App.xaml.cs
+++ CustomTitleBarWindowMVVM/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CustomTitleBarWindow
+namespace CustomTitleBarWindowMVVM
 {
     /// <summary>
     /// Interaction logic for App.xaml
