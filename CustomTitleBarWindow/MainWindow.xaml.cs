a5fff9bf3d0e0cd5e27ca6cd80c20eecd130291b
Jerry Virden <JerryVirden@outlook.com>
2023-11-13 22:44:21 -0600

    More tweeks: Sizing and position when maximized.  Window border.

--- CustomTitleBarWindow/MainWindow.xaml.cs
+++ CustomTitleBarWindow/MainWindow.xaml.cs
@@ -1,70 +1,151 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Interop;
 
-namespace CustomTitleBarWindow
+namespace CustomTitleBarWindow;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        RefreshMaximizeRestoreButton();
+    }
 
-        private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
-        {
-            this.WindowState = WindowState.Minimized;
-        }
+    private void OnMinimizeButtonClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
 
-        private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+    private void OnMaximizeRestoreButtonClick(object sender, RoutedEventArgs e)
+    {
+        WindowState ^= WindowState.Maximized;
+    }
+
+    private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void RefreshMaximizeRestoreButton()
+    {
+        if (WindowState == WindowState.Maximized)
         {
-            if (this.WindowState == WindowState.Maximized)
+            maximizeButton.Visibility = Visibility.Collapsed;
+            restoreButton.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            maximizeButton.Visibility = Visibility.Visible;
+            restoreButton.Visibility = Visibility.Collapsed;
+        }
+    }
+
+    private void Window_StateChanged(object sender, EventArgs e)
+    {
+        RefreshMaximizeRestoreButton();
+    }
+
+
+    // Maximized placement and sizing
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        ((HwndSource)PresentationSource.FromVisual(this)).AddHook(HookProc);
+    }
+
+    public static IntPtr HookProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+    {
+        if (msg == WM_GETMINMAXINFO)
+        {
+            // We need to tell the system what our size should be when maximized. Otherwise it will cover the whole screen,
+            // including the task bar.
+            MINMAXINFO mmi = (MINMAXINFO)Marshal.PtrToStructure(lParam, typeof(MINMAXINFO));
+
+            // Adjust the maximized size and position to fit the work area of the correct monitor
+            IntPtr monitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+
+            if (monitor != IntPtr.Zero)
             {
-                this.WindowState = WindowState.Normal;
+                MONITORINFO monitorInfo = new MONITORINFO();
+                monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+                GetMonitorInfo(monitor, ref monitorInfo);
+                RECT rcWorkArea = monitorInfo.rcWork;
+                RECT rcMonitorArea = monitorInfo.rcMonitor;
+                mmi.ptMaxPosition.X = Math.Abs(rcWorkArea.Left - rcMonitorArea.Left);
+                mmi.ptMaxPosition.Y = Math.Abs(rcWorkArea.Top - rcMonitorArea.Top);
+                mmi.ptMaxSize.X = Math.Abs(rcWorkArea.Right - rcWorkArea.Left);
+                mmi.ptMaxSize.Y = Math.Abs(rcWorkArea.Bottom - rcWorkArea.Top);
             }
-            else
-            {
-                this.WindowState = WindowState.Maximized;
-            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
         }
 
-        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        return IntPtr.Zero;
+    }
+
+    private const int WM_GETMINMAXINFO = 0x0024;
+
+    private const uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr MonitorFromWindow(IntPtr handle, uint flags);
+
+    [DllImport("user32.dll")]
+    private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
         {
-            this.Close();
+            this.Left = left;
+            this.Top = top;
+            this.Right = right;
+            this.Bottom = bottom;
         }
+    }
 
-        private void RefreshMaximizeRestoreButton()
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MONITORINFO
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public uint dwFlags;
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
         {
-            if (this.WindowState == WindowState.Maximized)
-            {
-                this.maximizeButton.Visibility = Visibility.Collapsed;
-                this.restoreButton.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                this.maximizeButton.Visibility = Visibility.Visible;
-                this.restoreButton.Visibility = Visibility.Collapsed;
-            }
+            this.X = x;
+            this.Y = y;
         }
+    }
 
-        private void Window_StateChanged(object sender, EventArgs e)
-        {
-            this.RefreshMaximizeRestoreButton();
-        }
-
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMaxTrackSize;
     }
 }
