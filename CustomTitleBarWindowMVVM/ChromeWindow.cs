1a4f87e089ccafad699e188cfc6978be5d5af006
Jerry Virden <JerryVirden@outlook.com>
2023-11-17 23:21:12 -0600

    Back to original data paths after a restart shuffled the thick lines back.

---
+++ CustomTitleBarWindowMVVM/ChromeWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace CustomTitleBarWindowMVVM;
+
+public class ChromeWindow : Window
+{
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        ((HwndSource)PresentationSource.FromVisual(this)).AddHook(User32Helpers.HookProc);
+    }
+}
--- CustomTitleBarWindowMVVM/MainWindow.xaml.cs
+++ CustomTitleBarWindowMVVM/MainWindow.xaml.cs
@@ -1,39 +1,19 @@
 ﻿using CustomTitleBarWindowMVVM.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace CustomTitleBarWindowMVVM
+namespace CustomTitleBarWindowMVVM;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            DataContext = new WindowViewModel(this);
-        }
-
-        protected override void OnSourceInitialized(EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-            ((HwndSource)PresentationSource.FromVisual(this)).AddHook(User32Helpers.HookProc);
-        }
+        DataContext = new WindowViewModel(this);
     }
 }
